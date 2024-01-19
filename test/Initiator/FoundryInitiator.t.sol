a362d5a95abbfd0caa784e580c78818fe80d8923
scab24 <git.seco@protonmail.com>
2024-01-19 17:38:58 +0100

    tests

--- test/Initiator/FoundryInitiator.t.sol
+++ test/Initiator/FoundryInitiator.t.sol
@@ -284,24 +284,32 @@ function testFail_initiatePayment_ExpiredSubscription() public {
     initiator.initiatePayment(subscriber);
 }
 
-function test_initiatePayment_ActiveSubscription() public {
-    address subscriber = holders[0];
-    uint256 amount = 1 ether;
-    uint256 validUntil = block.timestamp + 30 days;
-    uint256 paymentInterval = 10 days;
+    function test_initiatePayment_ActiveSubscription() public {
+        address subscriber = holders[0];
+        uint256 amount = 1 ether;
+        uint256 _validAfter = block.timestamp + 1 days;
+        uint256 validUntil = _validAfter + 10 days;
+        uint256 paymentInterval = 10 days;
 
-    // Registrar la suscripción como el suscriptor
-    vm.prank(subscriber);
-    initiator.registerSubscription(subscriber, amount, validUntil, paymentInterval, address(token));
+        // Registrar la suscripción
+        vm.prank(subscriber);
+        initiator.registerSubscription(subscriber, amount, validUntil, paymentInterval, address(token));
 
-    // Asegurarse de que estamos en un momento en el que la suscripción está activa y no ha expirado
-    vm.warp(block.timestamp + 1 days);
+        // Avanzar al momento en que la suscripción está activa pero no ha expirado
+        uint256 warpToTime = _validAfter + 10;
+        vm.warp(warpToTime);
 
-    // Intentar iniciar un pago (no debería fallar)
-    initiator.initiatePayment(subscriber);
-    
-    // Verificaciones adicionales pueden ser añadidas aquí si es necesario
-}
+        // Intentar iniciar un pago
+        vm.prank(subscriber);
+        bool success;
+        try initiator.initiatePayment(subscriber) {
+            success = true;
+        } catch {
+            success = false;
+        }
+
+        assertTrue(success, "La llamada a initiatePayment haber sido exitosa");
+    }
 
 
 /////////////////////////////////////////////////////////////////////////
@@ -368,7 +376,147 @@ function test_initiatePayment_ActiveSubscription() public {
         assertFalse(isSubscriberPresent, "Subscriber should be removed from the subscribers array");
     }
 
-// INVARIANT TEST 
+
+/////////////////////////////////////////////////////////////////////////
+// registerSubscription NoToken ERC20 => true
+/////////////////////////////////////////////////////////////////////////
+    function test_primer_failTokenFalse(
+        uint256 amount,
+        uint256 _validUntil,
+        uint256 paymentInterval,
+        address FalseToken) public {
+
+
+        address subscriber = holders[0];
+        amount = bound(amount, 1 ether, 1000 ether);
+        paymentInterval = bound(paymentInterval, 1 days, 365 days);
+        uint256 validUntil = block.timestamp + bound(_validUntil, 1 days, 365 days);
+        
+        vm.assume (FalseToken != address(token));
+        vm.assume(amount > 0 && paymentInterval > 0 && validUntil > block.timestamp);
+
+        // Registrar una suscripción
+        vm.prank(subscriber);
+        vm.expectRevert();
+        initiator.registerSubscription(subscriber, amount, validUntil, paymentInterval, FalseToken);
+
+    }
+
+/////////////////////////////////////////////////////////////////////////
+// Nunca se llama a _processNativePayment a no ser que sea address(0)
+/////////////////////////////////////////////////////////////////////////
+    function test_Fuzz_tinitiatePayment_ActiveSubscription(        
+            uint256 amount,
+            uint256 _validUntil,
+            uint256 paymentInterval,
+            address FalseToken) public {
+
+
+            address subscriber = holders[0];
+            uint256 _validAfter = block.timestamp + 1 days;
+            uint256 validUntil = _validAfter + 10 days;
+
+            amount = bound(amount, 1 ether, 1000 ether);
+            paymentInterval = bound(paymentInterval, 1 days, 365 days);
+
+            // Registrar la suscripción
+            vm.prank(subscriber);
+            initiator.registerSubscription(subscriber, amount, validUntil, paymentInterval, address(token));
+        ISubExecutor.SubStorage memory sub = initiator.getSubscription(subscriber);
+            assertEq(sub.amount, amount);
+            assertEq(sub.validUntil, validUntil);
+            assertEq(sub.paymentInterval, paymentInterval);
+            assertEq(sub.subscriber, subscriber);
+            assertEq(sub.initiator, address(initiator));
+            assertEq(sub.erc20Token, address(FalseToken));
+            assertEq(sub.erc20TokensValid, FalseToken != address(0));
+            // Avanzar al momento en que la suscripción está activa pero no ha expirado
+            uint256 warpToTime = _validAfter + 10;
+            vm.warp(warpToTime);
+
+            // Intentar iniciar un pago
+            vm.prank(subscriber);
+            bool success;
+            try initiator.initiatePayment(subscriber) {
+                success = true;
+            } catch {
+                success = false;
+            }
+
+            assertTrue(success, "La llamada a initiatePayment haber sido exitosa");
+        }
+
+/////////////////////////////////////////////////////////////////////////
+// Nunca se llama a _processNativePayment a no ser que sea address(0)
+/////////////////////////////////////////////////////////////////////////
+    function test_payment_validity_period(uint256 _amount, uint256 _paymentInterval) public {
+        if(_amount == 0 || _paymentInterval == 0) return;
+
+        uint256 _validAfter = block.timestamp + 1 days;
+        uint256 _validUntil = _validAfter + 10 days;
+        address subscriber = holders[0];
+
+        // Registering a subscription
+        vm.prank(subscriber);
+        initiator.registerSubscription(subscriber, _amount, _validUntil, _paymentInterval, address(token));
+
+        // Warp to a time before the subscription is valid
+        vm.warp(_validAfter - 10);
+        vm.prank(subscriber);
+        try initiator.initiatePayment(subscriber) {
+            // If this line is reached, the test should fail
+            assert(false);
+        } catch {
+            // Expected behavior, the transaction should revert
+        }
+
+        // Warp to a time after the subscription has expired
+        vm.warp(_validUntil + 10);
+        vm.prank(subscriber);
+        try initiator.initiatePayment(subscriber) {
+            // If this line is reached, the test should fail
+            assert(false);
+        } catch {
+            // Expected behavior, the transaction should revert
+        }
+
+        // Warp to a time within the valid range and ensure it doesn't revert
+        vm.warp(_validAfter + 1);
+        vm.prank(subscriber);
+        try initiator.initiatePayment(subscriber) {
+            // Expected behavior, the transaction should succeed
+        } catch {
+            assert(false);
+        }
+    }
+
+/////////////////////////////////////////////////////////////////////////
+// registerSubscription > 1
+/////////////////////////////////////////////////////////////////////////
+    function testFuzzMultipleSubscriptions() public {
+        address subscriber = holders[0];
+        uint256 iterations = 5; // Número de veces que quieres registrar al suscriptor
+
+        for (uint256 i = 0; i < iterations; i++) {
+            // Generar valores aleatorios para cada registro
+            uint256 amount = uint256(keccak256(abi.encodePacked(block.timestamp, subscriber, i))) % 100 ether;
+            uint256 validUntil = block.timestamp + (1 days + i * 1 days);
+            uint256 paymentInterval = (1 days + i * 1 hours);
+
+            // Asegurar que los valores son razonables
+            vm.assume(amount > 0);
+            vm.assume(validUntil > block.timestamp);
+            vm.assume(paymentInterval > 0);
+
+            // Registrar la suscripción
+            vm.prank(subscriber);
+            initiator.registerSubscription(subscriber, amount, validUntil, paymentInterval, address(token));
+            address[] memory registeredSubscribers = initiator.getSubscribers();
+            console.log("Subscriber i",registeredSubscribers[i]);
+            console.log("===================");
+            assertEq(registeredSubscribers[i], subscriber);
+        }
+    }
 
 
 
